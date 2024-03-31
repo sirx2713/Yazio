3a096f02b86f18f8f31b343cde1d60ebfa9e71d2
sirx2713 <bradx2713@gmail.com>
2024-03-31 05:19:21 +0200

    Initial Commit

---
+++ Yazio/YazioApp.swift
@@ -0,0 +1,17 @@
+//
+//  YazioApp.swift
+//  Yazio
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/31.
+//
+
+import SwiftUI
+
+@main
+struct YazioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
